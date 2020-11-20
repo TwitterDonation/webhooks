a887d89d0c053a8a63afe81735668667ff9bb472
Omar Aflak <aflakomar@gmail.com>
2020-11-21 00:30:32 +0100

    firestore admin: update user balance after paypal deposit

--- functions/paypal_controllers/events.js
+++ functions/paypal_controllers/events.js
@@ -1,6 +1,10 @@
+const firebaseAdmin = require('firebase-admin')
 const functions = require('firebase-functions')
 const paypal = require('paypal-rest-sdk')
+
 const config = functions.config()
+const app = firebaseAdmin.initializeApp()
+const db = app.firestore()
 
 paypal.configure({
     'mode': 'live',
@@ -21,18 +25,51 @@ const executePayment = async (paymentId, payerId) => {
     })
 }
 
+const updateUserBalance = async (userId, currency, total, fees) => {
+    const ref = db.collection('users').doc(userId)
+    return ref.get()
+        .then(doc => {
+            // TODO: currency conversion
+            const data = doc.data()
+            const currentBalance = data.balance ? data.balance : 0
+            return parseFloat((currentBalance + total - fees).toFixed(10))
+        })
+        .then(newBalance => {
+            return ref.set({
+                balance: newBalance,
+                currency: currency
+            }, {merge: true})
+        })
+}
+
 module.exports = async (request, response) => {
     const json = request.body
-    if (json.event_type == 'PAYMENTS.PAYMENT.CREATED') {
-        const paymentId = json.resource.id
-        const payerId = json.resource.payer.payer_info.payer_id
+    if (json.event_type != 'PAYMENTS.PAYMENT.CREATED') {
+        response.sendStatus(404)
+        return
+    }
+
+    const paymentId = json.resource.id
+    const payerId = json.resource.payer.payer_info.payer_id
+    try {
+        const payment = await executePayment(paymentId, payerId)
+        if (payment.httpStatusCode !== 200) {
+            response.status(500).send('Could not execute transaction')
+            return
+        }
+
+        const payerId = payment.payer.payer_info.payer_id
+        const sale = payment.transactions[0].related_resources[0].sale
+        const currency = sale.amount.currency
+        const total = parseFloat(sale.amount.total)
+        const fees = parseFloat(sale.transaction_fee.value)
         try {
-            const payment = await executePayment(paymentId, payerId)
-            response.status(200).send(payment)
+            await updateUserBalance(payerId, currency, total, fees)
+            response.status(200).end()
         } catch(error) {
             response.status(500).send(error)
         }
-    } else {
-        response.sendStatus(403)
+    } catch(error) {
+        response.status(500).send(error)
     }
 }
