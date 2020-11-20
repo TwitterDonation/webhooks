8584791b04077a032aa24f33f8c4fb4fff59c0dc
Omar Aflak <aflakomar@gmail.com>
2020-11-20 14:42:43 +0100

    splat code

--- functions/twitter_controllers/events.js
+++ functions/twitter_controllers/events.js
@@ -1,7 +1,4 @@
-const functions = require('firebase-functions')
-const config = functions.config()
-
-const re = new RegExp(/^@DonationBot \$(?<amount>\d+) @\S+$/)
+const re = new RegExp(/^@DonationBot (?<currency>[\$\€\£])(?<amount>\d+) @\S+$/)
 const currencyMap = {
     '$': 'USD',
     '€': 'EUR',
@@ -10,29 +7,35 @@ const currencyMap = {
 
 module.exports = (request, response) => {
     const json = request.body
-    functions.logger.log(json)
     if (json.tweet_create_events) {
         const object = json.tweet_create_events
         const accounts = object.entities.user_mentions
-        if (accounts.length == 2) {
-            const ids = accounts.filter(acc => acc.id_str !== json.for_user_id)
-            const recipientId = ids.pop()
-            if (ids.length == 0 && recipientId != object.user.id_str) {
-                const matches = re.exec(object.text)
-                if (matches) {
-                    const amount = matches.groups.amount
-                    const currency = currencyMap[matches.groups.currency]
-                    // Paypal: transfer `amount` of `currency` to `recipientId`
-                } else {
-                    // error tweet: wrong formatting
-                }
-            } else {
-                // error tweet: cannot donate to @DonationBot or to user itself
-            }
-        } else {
-            // error tweet: two many recipients
+        if (accounts.length !== 2) {
+            // error tweet: two many / not enough recipients
+            response.sendStatus(200)
+            return
         }
+
+        const ids = accounts.filter(acc => acc.id_str !== json.for_user_id)
+        const recipientId = ids.pop()
+        if (ids.length > 0 || !recipientId || recipientId == object.user.id_str) {
+            // error tweet: cannot donate to @DonationBot or to user itself
+            response.sendStatus(200)
+            return
+        }
+
+        const matches = re.exec(object.text)
+        if (!matches) {
+            // error tweet: wrong tweet formatting
+            response.sendStatus(200)
+            return
+        }
+
+        const amount = matches.groups.amount
+        const currency = currencyMap[matches.groups.currency]
+        // Paypal: transfer `amount` of `currency` to `recipientId`
+        response.sendStatus(200)
     } else {
-        response.status(200).end()
+        response.sendStatus(200)
     }
 }
