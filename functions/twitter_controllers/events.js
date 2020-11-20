f9f8c7b393d86fd6f3861cbe1b7321b87d0d9df8
Omar Aflak <aflakomar@gmail.com>
2020-11-20 18:11:35 +0100

    iterate over events

--- functions/twitter_controllers/events.js
+++ functions/twitter_controllers/events.js
@@ -40,64 +40,66 @@ const tweetReply = async (status, in_reply_to_status_id) => {
 
 module.exports = async (request, response) => {
     const json = request.body
+    const botId = json.for_user_id
     functions.logger.log(json)
 
     if (json.tweet_create_events) {
-        const object = json.tweet_create_events
-        const accounts = object.entities.user_mentions
-        const senderScreenName = object.user.screen_name
+        for (object of json.tweet_create_events) {
+            const accounts = object.entities.user_mentions
+            const senderScreenName = object.user.screen_name
 
-        if (accounts.length !== 2) {
+            if (accounts.length !== 2) {
+                try {
+                    const msg = `@${senderScreenName} You need to specify a valid recipient! Use the following syntax: "@DonationBot $5 @${senderScreenName}".`
+                    await tweetReply(msg, object.id_str)
+                } catch (error) {
+                    functions.logger.log(error)
+                } finally {
+                    response.sendStatus(200)
+                    continue
+                }
+            }
+
+            const ids = accounts.filter(acc => acc.id_str !== botId)
+            const recipientId = ids.pop()
+            if (ids.length > 0 || !recipientId || recipientId == object.user.id_str) {
+                try {
+                    const msg = `@${senderScreenName} You cannot donate to me (unfortunately) or to yourself...`
+                    await tweetReply(msg, object.id_str)
+                } catch (error) {
+                    functions.logger.log(error)
+                } finally {
+                    response.sendStatus(200)
+                    continue
+                }
+            }
+
+            const matches = re.exec(object.text)
+            if (!matches) {
+                try {
+                    const msg = `@${senderScreenName} Wrong Tweet formatting! Use the following syntax: "@DonationBot $5 @${senderScreenName}".`
+                    await tweetReply(msg, object.id_str)
+                } catch (error) {
+                    functions.logger.log(error)
+                } finally {
+                    response.sendStatus(200)
+                    continue
+                }
+            }
+
+            const amount = matches.groups.amount
+            const currency = currencyMap[matches.groups.currency]
+
             try {
-                const msg = `@${senderScreenName} You need to specify a valid recipient! Use the following syntax: "@DonationBot $5 @${senderScreenName}".`
+                // Paypal: transfer `amount` of `currency` to `recipientId`
+                const msg = `@${senderScreenName} Understood! 👌💸🎉`
                 await tweetReply(msg, object.id_str)
             } catch (error) {
                 functions.logger.log(error)
             } finally {
                 response.sendStatus(200)
-                return
+                continue
             }
-        }
-
-        const ids = accounts.filter(acc => acc.id_str !== json.for_user_id)
-        const recipientId = ids.pop()
-        if (ids.length > 0 || !recipientId || recipientId == object.user.id_str) {
-            try {
-                const msg = `@${senderScreenName} You cannot donate to me (unfortunately) or to yourself...`
-                await tweetReply(msg, object.id_str)
-            } catch (error) {
-                functions.logger.log(error)
-            } finally {
-                response.sendStatus(200)
-                return
-            }
-        }
-
-        const matches = re.exec(object.text)
-        if (!matches) {
-            try {
-                const msg = `@${senderScreenName} Wrong Tweet formatting! Use the following syntax: "@DonationBot $5 @${senderScreenName}".`
-                await tweetReply(msg, object.id_str)
-            } catch (error) {
-                functions.logger.log(error)
-            } finally {
-                response.sendStatus(200)
-                return
-            }
-        }
-
-        const amount = matches.groups.amount
-        const currency = currencyMap[matches.groups.currency]
-
-        try {
-            // Paypal: transfer `amount` of `currency` to `recipientId`
-            const msg = `@${senderScreenName} Understood! 👌💸🎉`
-            await tweetReply(msg, object.id_str)
-        } catch (error) {
-            functions.logger.log(error)
-        } finally {
-            response.sendStatus(200)
-            return
         }
     } else {
         response.sendStatus(200)
