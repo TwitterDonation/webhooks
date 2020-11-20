66cd8e73f842ecfdf610c391626a19be95159c6b
Omar Aflak <aflakomar@gmail.com>
2020-11-20 17:33:02 +0100

    added other twitter api keys

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Twitter #Codechella Hackathon 2020
 
 ```
-firebase functions:config:set twitter.api_key_secret="CONSUMER_SECRET"
+firebase functions:config:set twitter.consumer_key="CONSUMER_KEY"
+firebase functions:config:set twitter.consumer_secret="CONSUMER_SECRET"
+firebase functions:config:set twitter.token="TOKEN"
+firebase functions:config:set twitter.token_secret="TOKEN_SECRET"
 firebase functions:config:set paypal.client_id="CLIENT_ID"
 firebase functions:config:set paypal.client_secret="CLIENT_SECRET"
 firebase deploy
--- functions/twitter_controllers/crc.js
+++ functions/twitter_controllers/crc.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto')
 const config = require('firebase-functions').config()
-const CONSUMER_SECRET = config.twitter.api_key_secret
+const CONSUMER_SECRET = config.twitter.consumer_secret
 
 module.exports = (request, response) => {
     const crc = request.query.crc_token
@@ -8,7 +8,7 @@ module.exports = (request, response) => {
         const hmac = crypto.createHmac('sha256', CONSUMER_SECRET).update(crc).digest('base64')
         response.status(200).send({response_token: 'sha256=' + hmac})
     } else {
-        console.error("crc_token missing from request.")
+        console.error('crc_token missing from request.')
         response.sendStatus(400)
     }
 }
