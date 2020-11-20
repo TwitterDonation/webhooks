de86dab25d442b225b87cf15d3e83cef30f3fdcf
Omar Aflak <aflakomar@gmail.com>
2020-11-20 10:01:36 +0100

    fixed crc error

--- functions/twitter_controllers/crc.js
+++ functions/twitter_controllers/crc.js
@@ -1,10 +1,11 @@
 const crypto = require('crypto')
 const config = require('firebase-functions').config()
+const CONSUMER_SECRET = config.twitter.api_key_secret
 
 module.exports = (request, response) => {
     const crc = request.query.crc_token
     if (crc) {
-        const hmac = crypto.createHmac('sha256', twitter.api_key_secret).update(crc).digest('base64')
+        const hmac = crypto.createHmac('sha256', CONSUMER_SECRET).update(crc).digest('base64')
         response.status(200).send({response_token: 'sha256=' + hmac})
     } else {
         console.error("crc_token missing from request.")
